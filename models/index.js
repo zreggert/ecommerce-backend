fb595e8c1b86592313da449f22cfc8b855f8af34
elgalloloco <zreggert@gmail.com>
2021-05-14 14:42:49 -0400

    category routes finished and working properly

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tags, {
+Product.belongsToMany(Tag, {
   through: ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
